87d6c82e5248b3c7d9f61209bb455423c1b9dec3
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2022-11-14 17:39:57 +0900

    feat: 관련 데이터 객체 생성

---
+++ src/mock/products.ts
@@ -0,0 +1,111 @@
+export const products = [
+  {
+    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    image: "",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    rate: "3.9",
+    participant: 120,
+    price: 110,
+    category: "fashion",
+  },
+  {
+    name: "Mens Casual Premium Slim Fit T-Shirts",
+    image: "",
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    rate: "4,1",
+    participant: 259,
+    price: 22,
+    category: "fashion",
+  },
+  {
+    name: "Mens Cotton Jacket",
+    image: "",
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    rate: "4.7",
+    participant: 500,
+    price: 56,
+    category: "fashion",
+  },
+  {
+    name: "Mens Casual Slim Fit",
+    image: "",
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    rate: "2.1",
+    participant: 430,
+    price: 16,
+    category: "fashion",
+  },
+  {
+    name: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    image: "",
+    description:
+      "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
+    rate: "2.6",
+    participant: 235,
+    price: 57,
+    category: "fashion",
+  },
+  {
+    name: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    image: "",
+    description:
+      "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    rate: "2.9",
+    participant: 340,
+    price: 30,
+    category: "fashion",
+  },
+  {
+    name: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    image: "",
+    description:
+      "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+    rate: "3.8",
+    price: 40,
+    participant: 679,
+    category: "fashion",
+  },
+  {
+    name: "MBJ Women's Solid Short Sleeve Boat Neck V",
+    image: "",
+    description:
+      "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    rate: "4.7",
+    participant: 130,
+    price: 10,
+    category: "fashion",
+  },
+  {
+    name: "Opna Women's Short Sleeve Moisture",
+    image: "",
+    description:
+      "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+    rate: "4.5",
+    participant: 146,
+    price: 8,
+    category: "fashion",
+  },
+  {
+    name: "DANVOUY Womens T Shirt Casual Cotton Short",
+    image: "",
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    rate: "3.6",
+    participant: 145,
+    price: "13",
+    category: "fashion",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    rate: "",
+    participant: 0,
+    price: 0,
+    category: "",
+  },
+];
