f38a13c98b764510a03da9fe0754e16a940f8e82
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2022-11-14 17:38:48 +0900

    feat: Header 컴포넌트 생성

---
+++ src/component/header/header.styles.tsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 64px;
+  color: #fff;
+  background-color: #191a24;
+  z-index: 1;
+`;
+const HeaderInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1360px;
+  margin: 0 auto;
+`;
+const Gnb = styled.div`
+  display: flex;
+  align-items: center;
+  height: 64px;
+  margin-right: 16px;
+`;
+const Logo = styled.h1`
+  font-size: 18px;
+  font-weight: 600;
+`;
+const MenuList = styled.ul`
+  display: flex;
+  align-items: center;
+  height: 100%;
+
+  li:hover {
+    border-radius: 5px;
+    background-color: rgba(166, 173, 186, 0.2);
+  }
+
+  a {
+    display: block;
+    padding: 6px 12px;
+    font-size: 14px;
+  }
+`;
+const Snb = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ModeButton = styled.button`
+  width: 48px;
+  height: 48px;
+  color: #fff;
+`;
+const SearchForm = styled.form``;
+const SearchInput = styled.input`
+  width: 185px;
+  height: 48px;
+  padding: 0 16px;
+  font-size: 14px;
+  border-radius: 5px;
+  background-color: #4b5563;
+`;
+const CartButton = styled(ModeButton)`
+  margin-left: 4px;
+  &:hover {
+    border-radius: 5px;
+    background-color: rgba(166, 173, 186, 0.2);
+  }
+`;
+
+export {
+  HeaderWrapper,
+  HeaderInner,
+  Gnb,
+  Logo,
+  MenuList,
+  Snb,
+  ModeButton,
+  SearchForm,
+  SearchInput,
+  CartButton,
+};
--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  HiOutlineSun,
+  HiOutlineMoon,
+  HiOutlineShoppingBag,
+} from "react-icons/hi";
+import {
+  HeaderWrapper,
+  HeaderInner,
+  Gnb,
+  Logo,
+  MenuList,
+  Snb,
+  ModeButton,
+  SearchForm,
+  SearchInput,
+  CartButton,
+} from "./header.styles";
 
-const header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Header = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  const handleClick = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <HeaderWrapper>
+      <HeaderInner>
+        <Gnb>
+          <Logo>React Shop</Logo>
+
+          <MenuList>
+            <li>
+              <Link to="">패션</Link>
+            </li>
+            <li>
+              <Link to="">악세서리</Link>
+            </li>
+            <li>
+              <Link to="">디지털</Link>
+            </li>
+          </MenuList>
+        </Gnb>
+        <Snb>
+          <ModeButton onClick={handleClick}>
+            {isDark ? <HiOutlineSun size={24} /> : <HiOutlineMoon size={24} />}
+          </ModeButton>
+          <SearchForm>
+            <SearchInput type="text" placeholder="검색" />
+          </SearchForm>
+          <CartButton>
+            <HiOutlineShoppingBag size={24} />
+          </CartButton>
+        </Snb>
+      </HeaderInner>
+    </HeaderWrapper>
+  );
 };
 
-export default header;
+export default Header;
