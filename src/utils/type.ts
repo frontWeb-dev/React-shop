8be2ecebc60186b45e30db4f1795ed7aac71c150
frontweb-dev <frontend_web@kakao.com>
2022-12-03 20:09:58 +0900

    feat: interface 폴더 생성

---
+++ src/utils/type.ts
@@ -0,0 +1,49 @@
+export interface slickProps {
+  children: React.ReactNode;
+  className?: string;
+  autoplay?: boolean | number;
+  speed?: number;
+  loop?: boolean;
+}
+
+export interface itemsProps {
+  image: string;
+  name: string;
+  title: string;
+  text: string;
+  type: string;
+}
+
+export interface iconProps {
+  image: string;
+  name: string;
+  path: string;
+}
+
+export interface cardProps {
+  image: string;
+  name: string;
+}
+
+export interface itemsProps {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export interface dataProps {
+  data: itemsProps[];
+  category?: string;
+}
+
+export interface itemProps {
+  data: itemsProps;
+  category?: string;
+}
