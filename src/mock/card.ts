be8588849b85d68d23c70cd3febce048934ab1a2
frontweb-dev <frontend_web@kakao.com>
2022-12-04 18:07:47 +0900

    chore: mocks 이미지 경로 수정

--- src/mock/card.ts
+++ src/mock/card.ts
@@ -2,27 +2,27 @@ import { cardProps } from '../utils/type';
 
 export const cards: cardProps[] = [
   {
-    image: 'public/assets/asset 21.svg',
+    image: '/assets/asset 21.svg',
     name: 'visa',
   },
   {
-    image: 'public/assets/asset 22.svg',
+    image: '/assets/asset 22.svg',
     name: 'marster',
   },
   {
-    image: 'public/assets/asset 23.svg',
+    image: '/assets/asset 23.svg',
     name: 'amex',
   },
   {
-    image: 'public/assets/asset 24.svg',
+    image: '/assets/asset 24.svg',
     name: 'cardImage',
   },
   {
-    image: 'public/assets/asset 25.svg',
+    image: '/assets/asset 25.svg',
     name: 'cardImage',
   },
   {
-    image: 'public/assets/asset 26.svg',
+    image: '/assets/asset 26.svg',
     name: 'discover',
   },
 ];
--- src/mock/icons.ts
+++ src/mock/icons.ts
@@ -2,17 +2,17 @@ import { iconProps } from '../utils/type';
 
 export const icons: iconProps[] = [
   {
-    image: 'public/assets/asset 27.svg',
+    image: '/assets/asset 27.svg',
     name: 'Facebook',
     path: 'https://www.facebook.com/0base',
   },
   {
-    image: 'public/assets/asset 28.svg',
+    image: '/assets/asset 28.svg',
     name: 'Instagram',
     path: 'https://www.instagram.com/zerobase.official/',
   },
   {
-    image: 'public/assets/asset 29.svg',
+    image: '/assets/asset 29.svg',
     name: 'Githubk',
     path: 'https://github.com/oinochoe',
   },
