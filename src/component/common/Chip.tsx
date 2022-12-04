d0fb04e61839f79af99b6de5e97dee18aa45dd89
frontweb-dev <frontend_web@kakao.com>
2022-12-04 18:06:23 +0900

    feat: Chip 컴포넌트 추가

---
+++ src/component/common/Chip.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface chipProps {
+  children: string;
+}
+
+const Chip = (props: chipProps) => {
+  return <ChipWrapper>{props.children}</ChipWrapper>;
+};
+
+const ChipWrapper = styled.div`
+  width: 50px;
+  height: 20px;
+  line-height: 20px;
+  padding: 0 9px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 600;
+  background-color: #1fb2a6;
+  border-radius: 16px;
+`;
+
+export default Chip;
