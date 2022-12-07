6a077a005526883f7557991f327c4430da4a0444
frontweb-dev <frontend_web@kakao.com>
2022-12-08 00:45:58 +0900

    feat: suspense 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -17,17 +17,19 @@ import CartPage from './pages/CartPage';
 const App = () => {
   return (
     <RecoilRoot>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/:category' element={<ListPage />} />
-          <Route path='/cart' element={<CartPage />} />
-          <Route path='/product/:id' element={<DetailPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/:category' element={<ListPage />} />
+            <Route path='/cart' element={<CartPage />} />
+            <Route path='/product/:id' element={<DetailPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Suspense>
     </RecoilRoot>
   );
 };
